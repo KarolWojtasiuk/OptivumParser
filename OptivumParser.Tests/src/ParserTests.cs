31c655844168922f9e13fec85b39c7f46802bd0a
KarolWojtasiuk <karolwojtasiuk@gmail.com>
2020-03-13 22:25:02 +0100

    ✅ Update test plan URL

--- OptivumParser.Tests/src/ParserTests.cs
+++ OptivumParser.Tests/src/ParserTests.cs
@@ -4,11 +4,12 @@ namespace OptivumParser.Tests
 {
     public class ParserTests
     {
-        const string TestPlanUrl = "https://plan.ekonomikzamosc.pl/";
+        const string TestPlanUrl = "https://karolwojtasiuk.github.io/testLessonPlan/";
         [Fact]
         public void PlanValidatingTest()
         {
             var parser = new Parser();
+
             Assert.True(parser.IsValidPlan(TestPlanUrl).value);
             Assert.False(parser.IsValidPlan("https://google.pl/").value);
         }
@@ -17,7 +18,11 @@ namespace OptivumParser.Tests
         public void GettingClassIdsTest()
         {
             var parser = new Parser();
-            Assert.NotEmpty(parser.GetClassIds(TestPlanUrl));
+            var classes = parser.GetClassIds(TestPlanUrl);
+
+            Assert.NotEmpty(classes);
+            Assert.Equal("1", classes["1a"]);
+            Assert.Equal("24", classes["3bt 3"]);
         }
     }
 }
