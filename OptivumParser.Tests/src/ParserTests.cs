6b077aa204efcfcb770f36aa4ab3623ffd0cd6d4
KarolWojtasiuk <karolwojtasiuk@gmail.com>
2020-03-13 21:30:59 +0100

    ✨ Add getting class IDs functionality

--- OptivumParser.Tests/src/ParserTests.cs
+++ OptivumParser.Tests/src/ParserTests.cs
@@ -4,12 +4,20 @@ namespace OptivumParser.Tests
 {
     public class ParserTests
     {
+        const string TestPlanUrl = "https://plan.ekonomikzamosc.pl/";
         [Fact]
         public void PlanValidatingTest()
         {
             var parser = new Parser();
-            Assert.True(parser.IsValidPlan("https://plan.ekonomikzamosc.pl/").value);
+            Assert.True(parser.IsValidPlan(TestPlanUrl).value);
             Assert.False(parser.IsValidPlan("https://google.pl/").value);
+        }
+
+        [Fact]
+        public void GettingClassIdsTest()
+        {
+            var parser = new Parser();
+            Assert.NotEmpty(parser.GetClassIds(TestPlanUrl));
         }
     }
 }
