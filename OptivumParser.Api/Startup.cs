093c8abbf102b0a2edb41703337fee0f7811513f
KarolWojtasiuk <karolwojtasiuk@gmail.com>
2020-03-15 20:17:57 +0100

    ✨ Add auto generating description to Swagger

--- OptivumParser.Api/Startup.cs
+++ OptivumParser.Api/Startup.cs
@@ -1,3 +1,6 @@
+using System;
+using System.IO;
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -22,6 +25,9 @@ namespace OptivumParser.Api
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "OptivumParser.Api", Version = "v1" });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
             });
         }
 
