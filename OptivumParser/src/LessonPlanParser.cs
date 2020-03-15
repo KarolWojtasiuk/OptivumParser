2a097485d077be0fa4723e7500f443ea86b1d405
KarolWojtasiuk <karolwojtasiuk@gmail.com>
2020-03-15 12:53:01 +0100

    ⚗  Add entire lesson plan parser

--- OptivumParser/src/LessonPlanParser.cs
+++ OptivumParser/src/LessonPlanParser.cs
@@ -1,7 +1,40 @@
+using System.Collections.Generic;
+
 namespace OptivumParser
 {
-    public class LessonPlanParser
+    public static class LessonPlanParser
     {
+        /// <summary>
+        /// Experimental function, gets the whole lesson plan and parses, it may take a while.
+        /// </summary>
+        public static LessonPlan GetEntirePlan(PlanProvider provider)
+        {
+            var ids = ListParser.GetAllIds(provider);
+            var classes = ids.classes;
+            var teachers = ids.teachers;
+            var rooms = ids.rooms;
 
+            var lessons = new List<Lesson>();
+            foreach (var classId in classes)
+            {
+                lessons.AddRange(LessonParser.GetLessonsForClass(provider, classId.Value));
+            }
+            foreach (var teacherId in teachers)
+            {
+                lessons.AddRange(LessonParser.GetLessonsForTeacher(provider, teacherId.Value));
+            }
+            foreach (var roomId in rooms)
+            {
+                lessons.AddRange(LessonParser.GetLessonsForClass(provider, roomId.Value));
+            }
+
+            return new LessonPlan()
+            {
+                Classes = classes,
+                Teachers = teachers,
+                Rooms = rooms,
+                Lessons = lessons
+            };
+        }
     }
 }
