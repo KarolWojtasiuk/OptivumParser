22c8989ae412193cf91204fb633b0b8d7963feba
KarolWojtasiuk <karolwojtasiuk@gmail.com>
2020-03-15 18:47:35 +0100

    ✨ Add Swagger

--- OptivumParser.Api/Startup.cs
+++ OptivumParser.Api/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace OptivumParser.Api
 {
@@ -25,6 +19,10 @@ namespace OptivumParser.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "OptivumParser.Api", Version = "v1" });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -35,6 +33,13 @@ namespace OptivumParser.Api
             }
 
             app.UseRouting();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "OptivumParser.Api v1");
+                c.RoutePrefix = "";
+            });
 
             app.UseEndpoints(endpoints =>
             {
